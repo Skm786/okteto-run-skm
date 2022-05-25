881390011c216d3aead22a0b507e2b1e7dc0ae88
Skm786 <86302003+Skm786@users.noreply.github.com>
2022-05-25 13:17:32 +0530

    Update reqv.sh

--- reqv.sh
+++ reqv.sh
@@ -1,44 +1,7 @@
 rin() {
 pip install --upgrade pip
-pip install anytree
-pip install appdirs
-pip install aria2p
-pip install attrdict
-pip install beautifulsoup4
-pip install bencoding
-pip install cfscrape
-pip install feedparser
-pip install flask
-pip install google-api-python-client
-pip install google-auth-httplib2
-pip install google-auth-oauthlib
-pip install gunicorn
-pip install lk21
-pip install lxml
-pip install pillow
-pip install psutil
-pip install psycopg2-binary
-pip install pybase64
-pip install pyrogram
-pip install python-dotenv
-pip install python-magic
-pip install python-telegram-bot
-pip install qbittorrent-api
-pip install requests
-pip install speedtest-cli
-pip install six
-pip install telegraph
-pip install tenacity
-pip install tgCrypto
-pip install urllib3
-pip install yt-dlp
-pip install google-api-python-client
-pip install progress
-pip install progressbar2
-pip install httplib2shim
-pip install google_auth_oauthlib
-pip install pyrogram
-pip install megasdkrestclient
+pip install -r requirements.txt
+pip install -r requirements-cli.txt
 }
 dl() {
 apt update -y
@@ -51,6 +14,6 @@ rm -rf okteto-bot-anasty
 https://github.com/Skm786/okteto-bot-anasty
 cd okteto-bot-anasty
 rin
-bash start*
+bash start.sh
 }
 dl
